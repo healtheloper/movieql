db93a0753a577a7b38b6b27a6f56edf343b49cd3
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-02-23 23:34:33 +0900

    New Query Create  movie(:id)

--- graphql/db.js
+++ graphql/db.js
@@ -1,15 +1,22 @@
-import fetch from "node-fetch"
-const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2";
 
 export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL
-  if(limit>0){
-    REQUEST_URL += `limit=${limit}`
+  let REQUEST_URL = `${API_URL}/list_movies.json?`;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-  if(rating>0){
-    REQUEST_URL += `&minimum_rating=${rating}`
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
   return fetch(REQUEST_URL)
-  .then(res => res.json())
-  .then(json => json.data.movies)
-}
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
+
+export const getMovie = (id) => {
+  let REQUEST_URL = `${API_URL}/movie_details.json?movie_id=${id}`;
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movie);
+};
